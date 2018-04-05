0c324b0556a6c18487d7f928ef310d3b3c09a299
MBP04 <mbp04@Adminui-MacBook-Pro.local>
2018-04-05 17:02:12 +0900

    Added 'Movie' class to model.swift

--- NaverAPIExample/Model.swift
+++ NaverAPIExample/Model.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+class Movie {
+    var title:String
+    var link:String
+    var imageURL:String
+    var image:UIImage?
+    var pubDate:Int
+    var director:String
+    var actors:String
+    var userRating:Double
+    
+    init (title:String, link:String, imageURL:String, pubDate:Int, director:String, actors:String, userRating:Double) {
+        self.title = title
+        self.link = link
+        self.imageURL = imageURL
+        self.pubDate = pubDate
+        self.director = director
+        self.actors = actors
+        self.userRating = userRating
+    }
+}
