22d1b80b624d46586b51a02f85fb7a7d778b330e
MBP04 <mbp04@Adminui-MacBook-Pro.local>
2018-04-05 16:54:14 +0900

    Added View Controller and Model classes

---
+++ NaverAPIExample/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  NaverAPIExample
+//
+//  Created by MBP04 on 2018. 4. 5..
+//  Copyright © 2018년 codershigh. All rights reserved.
+//
+
+import Foundation
---
+++ NaverAPIExample/MovieDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieDetailViewController.swift
+//  NaverAPIExample
+//
+//  Created by MBP04 on 2018. 4. 5..
+//  Copyright © 2018년 codershigh. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
