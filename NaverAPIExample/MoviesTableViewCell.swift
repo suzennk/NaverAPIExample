2f00199b358280f1321d3282a1fd259393b43fe4
MBP04 <mbp04@Adminui-MacBook-Pro.local>
2018-04-05 17:27:10 +0900

    Added MoviesTableViewCell class and connected it to storyboard

---
+++ NaverAPIExample/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  NaverAPIExample
+//
+//  Created by MBP04 on 2018. 4. 5..
+//  Copyright © 2018년 codershigh. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
